09489b9c6d272a2b94585d64d8964b8b92db95e9
Сухарик <65870+suhr@users.noreply.github.com>
2022-10-11 23:41:28 +0300

    TODO

--- flake.nix
+++ flake.nix
@@ -1,55 +1,21 @@
-{
-  description = "Typed math book";
+{ description = "Application packaged using poetry2nix";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-  inputs.flake-utils.url = "github:numtide/flake-utils/master";
-  inputs.poetry2nix = {
-    inputs.flake-utils.follows = "flake-utils";
-    inputs.nixpkgs.follows = "nixpkgs";
-    url = "github:nix-community/poetry2nix/master";
-  };
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.poetry2nix =
+    { url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";      };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = {self, nixpkgs, flake-utils, poetry2nix}:
+    let out = system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ poetry2nix.overlay ];
-        };
-        poetryOverrides = self: super: {
-
-          # Refs https://github.com/nix-community/poetry2nix/issues/218#issuecomment-981615612
-          typing-extensions = super.typing-extensions.overridePythonAttrs (
-            old: {
-              buildInputs = (old.buildInputs or [ ]) ++ [ self.flit-core ];
-            }
-          );
-        };
-      in rec {
-        packages = flake-utils.lib.flattenTree {
-          tmath-pyenv = pkgs.poetry2nix.mkPoetryEnv {
-            projectDir = self;
-            python = pkgs.python39;
-            overrides = [
-              pkgs.poetry2nix.defaultPoetryOverrides
-              poetryOverrides
-            ];
-          };
-          # tmath-html = pkgs.stdenv.mkDerivation {
-          #   name = "tmath";
-          #   src = self;
-          #   buildInputs = [ packages.tmath-pyenv ];
-          #   buildPhase = ''
-          #     make html
-          #   '';
-          #   installPhase = ''
-          #     mkdir -p $out
-          #     cp -R build/html/* $out/
-          #   '';
-          # };
-        };
-        defaultPackage = packages.tmath-pyenv;
-      }
-    );
-
-}
+        pkgs = import nixpkgs {inherit system;};
+        python = pkgs.python39;
+        pythonEnv = pkgs.poetry2nix.mkPoetryEnv
+          { inherit python;
+            projectDir = ./.; };
+      in
+        { devShell = pkgs.mkShell
+          { buildInputs = [pythonEnv]; }; };
+    in
+      with flake-utils.lib; eachSystem defaultSystems out;    }
